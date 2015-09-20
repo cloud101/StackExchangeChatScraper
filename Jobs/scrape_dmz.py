ade9c87d1c35a4febc519ec3e5daa8f891785b8d
Lucas Kauffman <cloud101eu@gmail>
2015-09-20 11:18:00 -0700

    Updated the code to match the dict object

--- Jobs/scrape_dmz.py
+++ Jobs/scrape_dmz.py
@@ -4,7 +4,9 @@ import requests
 from Database.Elastic import ElasticManager
 
 scraper = TranscriptScraper(151)
+#keep a list which contains all URLs we need to fetch and process
 process_list = list()
+#keep a list of URLs which have already been processed so we do not fetch the same page twice
 process_list.append(scraper.get_first_day())
 processed_list = list()
 while process_list:
@@ -16,6 +18,7 @@ while process_list:
     for pager_url in scraper.get_pager(response.content):
         if pager_url not in processed_list:
             process_list.append(pager_url)
+    # a monologue can contain several messages
     monologues = scraper.extract_monologues(response.content)
     messages = scraper.extract_messages_from_monologues(monologues)
     ElasticManager.index_messages(messages)
