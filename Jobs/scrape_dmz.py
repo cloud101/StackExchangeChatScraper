0c344202bcb8cfe03573a0c29f833edd778e0092
Lucas Kauffman <lucas@cloud101.eu>
2015-09-21 16:00:59 -0400

    Updated the exception handling

--- Jobs/scrape_dmz.py
+++ Jobs/scrape_dmz.py
@@ -10,19 +10,20 @@ process_list = list()
 process_list.append(scraper.get_first_day())
 processed_list = list()
 while process_list:
-    url = process_list.pop()
-    print url
-    processed_list.append(url)
-    response = requests.get(url)
-    process_list.append(scraper.get_next_day(response.content))
-    for pager_url in scraper.get_pager(response.content):
-        if pager_url not in processed_list:
-            process_list.append(pager_url)
-    # a monologue can contain several messages
     try:
-        monologues = scraper.extract_monologues(response.content)
-        messages = scraper.extract_messages_from_monologues(monologues)
-        ElasticManager.index_messages(messages)
+         url = process_list.pop()
+         print url
+         processed_list.append(url)
+         response = requests.get(url)
+         process_list.append(scraper.get_next_day(response.content))
+         for pager_url in scraper.get_pager(response.content):
+             if pager_url not in processed_list:
+                 process_list.append(pager_url)
+    # a monologue can contain several messages
+    
+             monologues = scraper.extract_monologues(response.content)
+             messages = scraper.extract_messages_from_monologues(monologues)
+             ElasticManager.index_messages(messages)
     except Exception, e:
         print e
 
